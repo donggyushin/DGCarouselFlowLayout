436f9843ab5ddd6c62d7e783587477ed05bd7e82
신동규 <donggyu9410@gmail.com>
2022-01-21 16:41:34 +0900

    init package

---
+++ Tests/DGCarouselFlowLayoutTests/DGCarouselFlowLayoutTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DGCarouselFlowLayout
+
+final class DGCarouselFlowLayoutTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DGCarouselFlowLayout().text, "Hello, World!")
+    }
+}
